c476dea984320ce3004038abb11eb0fde8e1ce97
AvinashAhire19 <avinash.ahire199@gmail.com>
2022-06-22 15:32:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Serviecs from './Serviecs';
+
+function App() {
+  return (
+    <div className="App">
+     <Serviecs/>
+    </div>
+  );
+}
+
+export default App;
